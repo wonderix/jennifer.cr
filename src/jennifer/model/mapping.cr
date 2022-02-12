a8dff2593892c3972fc99250dd23d075f88bfbdd
wonderix <wonderix@googlemail.com>
2022-02-12 16:12:25 +0100

    Fix DataTypeCasting

--- src/jennifer/model/mapping.cr
+++ src/jennifer/model/mapping.cr
@@ -435,6 +435,8 @@ module Jennifer
               %casted_var{key.id} =
                 {% if value[:parsed_type] =~ /String/ %}
                   %var{key.id}
+                {% elsif value[:parsed_type] =~ /Int32/ %}
+                  (%var{key.id}.is_a?(Int64) ? %var{key.id}.to_i32 : %var{key.id})
                 {% else %}
                   (%var{key.id}.is_a?(String) ? self.class.coerce_{{key.id}}(%var{key.id}) : %var{key.id})
                 {% end %}
